6ab50fc996218908f5997cfff1cc85997f803148
CipherVC <rafaelvc2806lf@gmail.com>
2022-11-23 20:55:15 -0600

    added profesor mantenimiento

---
+++ UI.WebMatricula3C2022/Logica/LnProfesor.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+
+namespace UI.WebMatricula3C2022.Logica
+{
+    public class LnProfesor
+    {
+        LnConsumoAPI lnConsumoAPI = new LnConsumoAPI();
+
+
+        public async Task<Models.Profesor.Salida.VerTodosProfesores> VerTodosProfesores(Models.Profesor.Entrada.VerTodosProfesores oInsumos,string token) {
+            string encabezado = "Profesor/VerTodosProfesores";
+            string cuerpo = JsonConvert.SerializeObject(oInsumos);
+            string respuesta = await lnConsumoAPI.ConsumirAPI(encabezado, cuerpo, token);
+            return JsonConvert.DeserializeObject<Models.Profesor.Salida.VerTodosProfesores>(respuesta);
+        }
+        public async Task<Models.Profesor.Salida.AgregarProfesor> AgregarProfesor(Models.Profesor.Entrada.AgregarProfesor pDatos, string token) {
+            string encabezado = "Profesor/AgregarProfesor";
+            string cuerpo=JsonConvert.SerializeObject(pDatos);
+            string respuesta=await lnConsumoAPI.ConsumirAPI(encabezado,cuerpo,token);
+            return JsonConvert.DeserializeObject<Models.Profesor.Salida.AgregarProfesor>(respuesta);
+        
+        }
+        public async Task<Models.Profesor.Salida.EditarProfesor> EditarProfesor(Models.Profesor.Entrada.EditarProfesor pDatos, string token)
+        {
+            string encabezado = "Profesor/EditarProfesor";
+            string cuerpo = JsonConvert.SerializeObject(pDatos);
+            string respuesta = await lnConsumoAPI.ConsumirAPI(encabezado, cuerpo, token);
+            return JsonConvert.DeserializeObject<Models.Profesor.Salida.EditarProfesor>(respuesta);
+
+        }
+        public async Task<Models.Profesor.Salida.EliminarProfesor> EliminarProfesor(Models.Profesor.Entrada.EliminarProfesor pDatos, string token)
+        {
+            string encabezado = "Profesor/EliminarProfesor";
+            string cuerpo = JsonConvert.SerializeObject(pDatos);
+            string respuesta = await lnConsumoAPI.ConsumirAPI(encabezado, cuerpo, token);
+            return JsonConvert.DeserializeObject<Models.Profesor.Salida.EliminarProfesor>(respuesta);
+
+        }
+        public async Task<Models.Profesor.Salida.VerDetalleProfesor> VerDetalleProfesor(Models.Profesor.Entrada.VerDetalleProfesor pDatos, string token)
+        {
+            string encabezado = "Profesor/VerDetalleProfesor";
+            string cuerpo = JsonConvert.SerializeObject(pDatos);
+            string respuesta = await lnConsumoAPI.ConsumirAPI(encabezado, cuerpo, token);
+            return JsonConvert.DeserializeObject<Models.Profesor.Salida.VerDetalleProfesor>(respuesta);
+
+        }
+
+    }
+}
---
+++ UI.WebMatricula3C2022/Models/Profesor/Entrada/VerTodosProfesores.cs
@@ -0,0 +1,28 @@
+﻿namespace UI.WebMatricula3C2022.Models.Profesor.Entrada
+{
+    public class VerTodosProfesores : General.EntradaAPI
+    {
+        public List<DatosProfesor> ListaProfesores { get; set; }
+
+
+
+        public VerTodosProfesores()
+        {
+            ListaProfesores = new List<DatosProfesor>();
+        }
+    }
+
+
+
+    public class DatosProfesor
+    {
+        public int Codigo { get; set; }
+        public string Identificacion { get; set; }
+        public string NombreCompleto { get; set; }
+        public string Especialidad { get; set; }
+        public string Telefono { get; set; }
+        public string Direccion { get; set; }
+        public int CodigoCurso { get; set; }
+        public int CodigoDepartamento { get; set; }
+    }
+}
